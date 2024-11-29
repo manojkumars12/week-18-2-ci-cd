42eed97fac7dce956815ea5d932e04680120cf9c
Manoj <manojskumar3122@gmail.com>
2024-11-29 23:14:14 +0530

    changed index.ts

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    //pr from feature-branch to master with CI
+    //TODO:
     const paymentInformation: {
         token: string;
         userId: string;
